1db5c4c7c7cf3cebc1f167473ee1248afd95da4c
AngelaSolomons <angelasolomons03@gmail.com>
2026-09-14 22:28:29 +0200

    Made navbar sticky, and updated BookFormView.vue css

--- src/App.vue
+++ src/App.vue
@@ -139,14 +139,14 @@ body {
     width: 100%;
     min-height: 100%;
     height: auto !important;
-    overflow-x: hidden !important;
-    overflow-y: auto !important;
+    /* overflow-x: hidden !important;
+    overflow-y: auto !important; */
 }
 
 #app {
     width: 100%;
     min-height: 100vh;
     height: auto !important;
-    overflow: visible !important;
+    /* overflow: visible !important; */
 }
 </style>
