e6cb50758f837983609563d5646e4b9a2cd006c2
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-09-15 09:36:57 +0200

    Added the sidebar for the admin page

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,5 +1,25 @@
-<template></template>
+<template> 
+    <div class="layout">
 
-<script></script>
+        <aside class="sidebar">
+            <div class="brand">Yenza Admin</div>
+            <li><a class="nav-item" href="#">Dashboard</a></li>
+            <li><a class="nav-item" href="#">Users</a></li>
+            <li><a class="nav-item" href="#">Bookings</a></li>
+            <li><a class="nav-item" href="#">Verifications</a></li>
+            <li><a class="nav-item" href="#">Reports</a></li>
+            <li><a class="nav-item" href="#">Reviews</a></li>
+            <li><a class="nav-item" href="#">Settings</a></li>
+            
+        </aside>
 
-<style></style>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
