48269aedf676ed407bc760fa48f2a5c0b10b2590
Onela0220 <knonhlanhla585@gmail.com>
2026-09-18 22:51:44 +0200

    Wired email OTP verification to frontend

--- frontend/src/api/verification.js
+++ frontend/src/api/verification.js
@@ -1,6 +1,16 @@
-import api from './api'
+import api from "./api";
 
-export const submitExperience= async (data) => {
-    const response = await api.post('/verifications/experience', data)
-    return response.data
-}
+export const submitExperience = async (data) => {
+  const response = await api.post("/verifications/experience", data);
+  return response.data;
+};
+
+export const sendOtp = async (data) => {
+  const response = await api.post("/verifications/send-otp", data);
+  return response.data;
+};
+
+export const verifyOtp = async (data) => {
+  const response = await api.post("/verifications/verify-otp", data);
+  return response.data;
+};
